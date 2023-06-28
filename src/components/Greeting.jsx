9391e48c697c37cad25b3541a4b2257a321e9bbf
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-06-28 12:13:02 -0400

    made my first component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+function Greeting(){
+    const name = "Andrew";
+    return (
+        <>
+            <h1>Hello {name}</h1>
+            <h2>We're glad you're here!</h2>
+        </>
+    )
+
+}
+
+export default Greeting;
